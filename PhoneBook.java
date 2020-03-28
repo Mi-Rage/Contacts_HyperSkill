8450ff07fbb859738e396630f77c4fcc94b1960c
Igor Chikhachev <59646864+Mi-Rage@users.noreply.github.com>
2020-03-28 13:56:49 +0300

    Созданы первичные варианты всех методов.

--- PhoneBook.java
+++ PhoneBook.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class PhoneBook {
 
     List<Person> storage = new ArrayList<>();
@@ -13,16 +14,18 @@ public class PhoneBook {
 
         while (true) {
             System.out.print("Enter action (add, remove, edit, count, list, exit): ");
-            String choice = scanner.next();
+            String choice = scanner.nextLine();
             switch (choice) {
                 case ("add"):
                     addPerson();
                     break;
                 case ("remove"):
-                    //do remove
+                    listPerson();
+                    removePerson();
                     break;
                 case ("edit"):
-                    //do edit
+                    listPerson();
+                    editPerson();
                     break;
                 case ("count"):
                     countPerson();
@@ -38,16 +41,29 @@ public class PhoneBook {
 
     public void addPerson() {
         Person person = new Person();
-        System.out.println("Enter the name of the person:");
-        person.setName(scanner.next());
-        System.out.println("Enter the surname of the person:");
-        person.setSurname(scanner.next());
-        System.out.println("Enter the number:");
-        person.setPhone(scanner.next());
+        System.out.print("Enter the name of the person: ");
+        person.setName(scanner.nextLine());
+        System.out.print("Enter the surname of the person: ");
+        person.setSurname(scanner.nextLine());
+        System.out.print("Enter the number: ");
+        String number = scanner.nextLine();
+        if (checkNumber(number)) {
+            person.setPhone(number);
+        } else {
+            System.out.println("Wrong number format!");
+            person.setPhone("[no number]");
+        }
         storage.add(person);
+        System.out.println("A record created!");
+    }
 
-        System.out.println("A record created!\n" +
-                "A Phone Book with a single record created!");
+    private boolean checkNumber(String number) {
+        //^\+?[\d]\s[-\(]?\d{3}\)?[\s-]{1}\d{3}[\s-]{1}\d{3}[\s-]{1}[0-9a-zA-Z]{4}$
+        //^[(]?\d{3}\)?[\s-]{1}\d{3}[\s-]{1}\d{3}[\s-]{1}[0-9a-zA-Z]{3,4}$
+
+        final String regex1 = "^\\+?[\\d]\\s[-(]?\\d{3}\\)?[\\s-]\\d{3}[\\s-]\\d{3}[\\s-][0-9a-zA-Z]{4}$";
+        final String regex2 = "^[(]?\\d{3}\\)?[\\s-]\\d{3}[\\s-]\\d{3}[\\s-][0-9a-zA-Z]{3,4}$";
+        return number.matches(regex1) || number.matches(regex2);
     }
 
     public void countPerson() {
@@ -58,8 +74,56 @@ public class PhoneBook {
         for (int i = 0; i < storage.size(); i++) {
             System.out.println((i + 1) + ". "
                     + storage.get(i).getName() + " "
-                    + storage.get(i).getSurname() + " "
+                    + storage.get(i).getSurname() + ", "
                     + storage.get(i).getPhone());
         }
+    }
+
+    public void removePerson(){
+        if (storage.size() == 0) {
+            System.out.println("No records to remove!");
+            return;
+        }
+        System.out.print("Select a record: ");
+        int selectRec = scanner.nextInt();
+        if (selectRec > storage.size()) {
+            System.out.println("ERROR! Select record biggest then capacity");
+            return;
+        }
+        storage.remove(selectRec - 1);
+        System.out.println("The record removed!");
+    }
+
+    public void editPerson() {
+        System.out.print("Select a record: ");
+        int record = scanner.nextInt();
+        if (record > storage.size()) {
+            System.out.println("ERROR! record has bigeer!");
+            return;
+        }
+        System.out.print("Select a field (name, surname, number): ");
+        scanner.nextLine();
+        String field = scanner.nextLine();
+        switch (field) {
+            case ("name") :
+                System.out.print("Enter name: ");
+                storage.get(record-1).setName(scanner.nextLine());
+                break;
+            case ("surname") :
+                System.out.print("Enter surname: ");
+                storage.get(record-1).setSurname(scanner.nextLine());
+                break;
+            case ("number") :
+                System.out.print("Enter number: ");
+                String number = scanner.nextLine();
+                if (checkNumber(number)) {
+                    storage.get(record-1).setPhone(number);
+                } else {
+                    System.out.println("Wrong number format!");
+                    storage.get(record-1).setPhone("[no number]");
+                }
+                break;
+        }
+        System.out.println("The record updated!");
     }
 }
